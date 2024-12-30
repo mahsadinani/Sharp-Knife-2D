52976b06c8dd0167b4ecff7d4b3168cadab139ce
mahsadinani <mahsadinani80@gmail.com>
2024-12-30 14:43:06 +0330

    GameOver + KnifeManager + Colorize the knife Icons

--- Assets/Scripts/KnifeController.cs
+++ Assets/Scripts/KnifeController.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class KnifeController : MonoBehaviour
 {
+    private KnifeManager knifeManager;
     private Rigidbody2D KnifeRigidBody;
     [SerializeField] private float MoveSpeed;
     private bool CanShoot;
@@ -30,11 +32,14 @@ public class KnifeController : MonoBehaviour
         if (CanShoot)
         {
             KnifeRigidBody.AddForce(Vector2.up * MoveSpeed * Time.fixedDeltaTime);
+            knifeManager.setDisableKnifeIconColor();
+
         }
     }
     private void GetComponentValues()
     {
         KnifeRigidBody = GetComponent<Rigidbody2D>();
+        knifeManager= GameObject.FindObjectOfType<KnifeManager>();
     }
     private void OnCollisionEnter2D(Collision2D other)
     {
@@ -43,8 +48,12 @@ public class KnifeController : MonoBehaviour
             CanShoot = false;
             KnifeRigidBody.isKinematic = true;
             transform.SetParent(other.gameObject.transform);
+            knifeManager.SetActiveKnife();
         }
-
+        if (other.gameObject.CompareTag("Knife"))
+        {
+            SceneManager.LoadScene(0);
+        }
 
     }
 }
--- Assets/Scripts/KnifeManager.cs
+++ Assets/Scripts/KnifeManager.cs
@@ -1,30 +1,61 @@
 using System.Collections.Generic;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class KnifeManager : MonoBehaviour
 {
     [SerializeField] private List<GameObject> KnifeList = new List<GameObject>();
+    [SerializeField] private List<GameObject> KnifeIconList = new List<GameObject>();
+
     [SerializeField] private GameObject KnifePrefab;
+    [SerializeField] private GameObject KnifeIconPrefab;
+    [SerializeField] private Color activeColor;
+    [SerializeField] private Color deActiveColor;
+    [SerializeField] private Vector2 knifeIconPosition;
     [SerializeField] private int KnifeCount;
-    private int knifeIndex;
+    private int knifeIndex=0;
     private void Start()
     {
         CreateKnives();
+        createKnifeIcons();
     }
     private void CreateKnives()
     {
         for(int i=0; i<KnifeCount; i++)
         {
-            GameObject newKnife = Instantiate(KnifePrefab,transform.position,Quaternion.identity);
+            // harchizi ra ba 3 soal misazad : chi ? koja ? chejori becharkhoonamesh ? 
+            GameObject newKnife = Instantiate(KnifePrefab,transform.position,quaternion.identity);
             newKnife.SetActive(false);
             KnifeList.Add(newKnife);
         }
         KnifeList[0].SetActive(true);
     }
+    public void setDisableKnifeIconColor()
+    {
+        KnifeIconList[(KnifeIconList.Count-1) - knifeIndex].GetComponent<SpriteRenderer>().color = deActiveColor;
+    }
+    private void createKnifeIcons()
+    {
+        for(int i =0 ; i< KnifeCount ; i++)
+        {
+            GameObject newKnifeIcon = Instantiate(KnifeIconPrefab,knifeIconPosition,KnifeIconPrefab.transform.rotation);
+            // Quaternion.identity = saaf mikhore
+            newKnifeIcon.GetComponent<SpriteRenderer>().color=activeColor; 
+            knifeIconPosition.y += 0.5f;
+            KnifeIconList.Add(newKnifeIcon);
+        }
+    }
+
+
+
     public void SetActiveKnife()
     {
-        
+        if(knifeIndex < KnifeCount )
+        {
+            knifeIndex++;
+            KnifeList[knifeIndex].SetActive(true);
+        }
     }
 
 }
